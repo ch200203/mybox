7e965ae1d30a47997c3bfc249bf6f0a083d6a97b
ch200203 <ch200205@naver.com>
2023-08-14 15:17:42 +0900

    chore : folder 엔티티 생성

---
+++ src/main/java/com/numble/mybox/mybox/folder/domain/Folder.java
@@ -0,0 +1,46 @@
+package com.numble.mybox.mybox.folder.domain;
+
+import com.numble.mybox.mybox.user.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "folder_id")
+    private Long folderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "folder_name", nullable = false, length = 255)
+    private String folderName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_parent_id", referencedColumnName = "folder_id")
+    private Folder parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Folder> children = new ArrayList<>();
+
+}
